c336f80acdd2de6d2b84030795802bc77751562d
Mathias Bynens <mathias@qiwi.be>
2019-02-14 16:19:51 +0100

    Stop actively supporting Node.js v8
    As documented, jsvu follows the latest active LTS version of Node.
    
    Node.js v10 has been in active LTS status since October 2018.
    https://github.com/nodejs/Release#release-schedule

--- shared/installer.js
+++ shared/installer.js
@@ -133,7 +133,7 @@ class Installer {
 			`%~dp0${this.targetRelPath}` :
 			this.targetPath;
 		const contents = generateScript(wrapperPath)
-			.trimLeft() // TODO: Use `trimStart` once Node.js v10 hits LTS.
+			.trimStart()
 			.replace(/\t/g, '');
 		fse.removeSync(to);
 		fse.writeFileSync(to, contents);
