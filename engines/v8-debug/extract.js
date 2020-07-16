d325c042d908977fc5bb9d9e54ca2fd6e96f115c
Mathias Bynens <mathias@qiwi.be>
2020-07-16 16:37:35 +0200

    Handle natives_blob.bin for v8-debug as well (#95)
    Closes #94.

--- engines/v8-debug/extract.js
+++ engines/v8-debug/extract.js
@@ -30,7 +30,7 @@ const extract = ({ filePath, binary, os }) => {
 			path: tmpPath,
 		});
 		installer.installLibrary('icudtl.dat');
-		installer.installLibrary('natives_blob.bin');
+		const hasNativesBlob = installer.installLibrary('natives_blob.bin');
 		installer.installLibrary('snapshot_blob.bin');
 		switch (os) {
 			case 'mac64': {
@@ -39,9 +39,10 @@ const extract = ({ filePath, binary, os }) => {
 				installer.installScript({
 					name: binary,
 					generateScript: (targetPath) => {
+						const nativesBlobArg = hasNativesBlob ? ` --natives_blob="${targetPath}/natives_blob.bin"` : '';
 						return `
 							#!/usr/bin/env bash
-							"${targetPath}/${binary}" --natives_blob="${targetPath}/natives_blob.bin" --snapshot_blob="${targetPath}/snapshot_blob.bin" "$@"
+							"${targetPath}/${binary}"${nativesBlobArg} --snapshot_blob="${targetPath}/snapshot_blob.bin" "$@"
 						`;
 					}
 				});
@@ -54,9 +55,10 @@ const extract = ({ filePath, binary, os }) => {
 				installer.installScript({
 					name: binary,
 					generateScript: (targetPath) => {
+						const nativesBlobArg = hasNativesBlob ? ` --natives_blob="${targetPath}/natives_blob.bin"` : '';
 						return `
 							#!/usr/bin/env bash
-							"${targetPath}/${binary}" --natives_blob="${targetPath}/natives_blob.bin" --snapshot_blob="${targetPath}/snapshot_blob.bin" "$@"
+							"${targetPath}/${binary}"${nativesBlobArg} --snapshot_blob="${targetPath}/snapshot_blob.bin" "$@"
 						`;
 					}
 				});
@@ -72,9 +74,10 @@ const extract = ({ filePath, binary, os }) => {
 				installer.installScript({
 					name: `${binary}.cmd`,
 					generateScript: (targetPath) => {
+						const nativesBlobArg = hasNativesBlob ? ` --natives_blob="${targetPath}\\natives_blob.bin"` : '';
 						return `
 							@echo off
-							"${targetPath}\\${binary}.exe" --natives_blob="${targetPath}\\natives_blob.bin" --snapshot_blob="${targetPath}\\snapshot_blob.bin" %*
+							"${targetPath}\\${binary}.exe"${nativesBlobArg} --snapshot_blob="${targetPath}\\snapshot_blob.bin" %*
 						`;
 					}
 				});
