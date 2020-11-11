eef367af3b4b1206e6a34f315d472c480a60d501
Serhii Ivanenko <serhii.ivanenko@oracle.com>
2020-11-11 15:55:05 +0200

    Add support for GraalJS engine (#92)

---
+++ engines/graaljs/get-latest-version.js
@@ -0,0 +1,32 @@
+// Copyright 2020 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the “License”);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// <https://apache.org/licenses/LICENSE-2.0>.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const get = require('../../shared/get.js');
+
+const getLatestVersion = () => {
+	const url = 'https://github.com/graalvm/graaljs/releases';
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await get(url);
+			const regex = /href="\/graalvm\/graaljs\/releases\/tag\/vm-([^"]+)">Graal/;
+			const version = regex.exec(response.body)[1];
+			resolve(version);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+module.exports = getLatestVersion;
