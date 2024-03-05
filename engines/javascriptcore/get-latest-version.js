823d0f2ddc1b414613306871b1073588e305c369
Jack Works <jackworks@protonmail.com>
2024-03-05 23:01:14 +0800

    add macOS Sonoma in javascriptcore download
    close #144

--- engines/javascriptcore/get-latest-version.js
+++ engines/javascriptcore/get-latest-version.js
@@ -71,6 +71,11 @@ const getLatestVersion = (os) => {
 				// https://build.webkit.org/#/builders/368
 				// This publishes universal x86_64 + arm64 binaries.
 				return getLatestRevisionFromBuilder(368);
+			} else if (name === 'sonoma') {
+				// Builder name: Apple-Monterey-Release-Build
+				// https://build.webkit.org/#/builders/938
+				// This publishes universal x86_64 + arm64 binaries.
+				return getLatestRevisionFromBuilder(938);
 			} else {
 				throw new Error(`Unknown MacOS name: ${name}.`);
 			}
